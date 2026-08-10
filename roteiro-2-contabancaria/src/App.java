cdfb50dcb405ae683a5a88f41fbe1d77fe9548dc
Rodrigo Goulart <rodrigo.goulart@gmail.com>
2026-08-10 20:34:26 -0300

    ini:commit inicial

---
+++ roteiro-2-contabancaria/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Conta c1 = new Conta();
+        //c1.setNumero("1234");
+        c1.numero = "Rodrigo";
+        System.out.println(c1.getNumero());
+        Conta c2 = new Conta();
+        c2.setNumero("4321");
+        System.out.println(c2.getNumero());
+        
+    }
+}
---
+++ roteiro-2-contabancaria/src/Conta.java
@@ -0,0 +1,22 @@
+public class Conta {
+
+    String numero;
+    private double saldo;
+    
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+    
+    
+}
